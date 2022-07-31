ae2cde5dd8ac42783e1359210575b0fcb3889535
KhelemeliaOleksii <xvdm@ukr.net>
2022-07-31 13:12:52 +0300

    first lesssons

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('../config/db');
+
+require('colors');
+require('../config/setEnvVars');
+
+const { PORT } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use('/api/v1', require('./routes/carsRoutes'));
+
+// connectDB();
+(async () => { await connectDB() })();
+
+app.use('*', (req, res, _) => {
+    res.status(404).json({
+        message: "Not Found"
+    });
+})
+app.use(require('./middlewares/errorHandler'));
+
+app.listen(PORT, () => {
+    console.log(`Serer is running on port ${PORT}`.cyan.italic.underline);
+})
+
---
+++ config/db.js
@@ -0,0 +1,13 @@
+
+const { connect } = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const db = await connect(process.env.MONGODB_URI);
+        console.log(`MongoDB is connected on HOST ${db.connection.host} on port ${db.connection.port} DB_name ${db.connection.name}`.cyan.italic.underline);
+    } catch (error) {
+        console.log(error.message.red);
+    }
+}
+
+module.exports = connectDB;
